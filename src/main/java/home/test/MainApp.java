3798430e033def8691ca4937f70506a7df769514
Dmitri Baskakov <dmitri.baskakov.git@gmail.com>
2021-01-14 11:28:30 +0700

    Add test swap sting

--- src/main/java/home/test/MainApp.java
+++ src/main/java/home/test/MainApp.java
@@ -7,12 +7,38 @@ import org.apache.camel.main.Main;
  */
 public class MainApp {
 
-    /**
-     * A main() so we can easily run these routing rules in our IDE
-     */
     public static void main(String... args) throws Exception {
         System.out.println("Hello world !");
+
+        StringWraper a = new StringWraper("aaa");
+        System.out.print("source a : ");
+        System.out.println(a.string);
+        StringWraper b = new StringWraper("bbb");
+        System.out.print("source b : ");
+        System.out.println(b.string);
+
+        Swap(a, b);
+        System.out.println("swap a <-> b");
+        System.out.print("result a : ");
+        System.out.println(a.string);
+        System.out.print("result b : ");
+        System.out.println(b.string);
     }
 
+
+    public static class StringWraper {
+        public String string;
+        StringWraper (String in) {
+            string = in;
+        }
+    }
+
+    public static void Swap (StringWraper a, StringWraper b) {
+        String c;
+        c = a.string;
+        a.string = b.string;
+        b.string = c;
+    }
 }
+
 
