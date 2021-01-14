f9683052203cbaac4297c1d19f93f0b2ef6e3b35
Dmitri Baskakov <dmitri.baskakov.git@gmail.com>
2021-01-14 09:16:48 +0700

    Set MainApp

--- src/main/java/home/test/MainApp.java
+++ src/main/java/home/test/MainApp.java
@@ -11,9 +11,7 @@ public class MainApp {
      * A main() so we can easily run these routing rules in our IDE
      */
     public static void main(String... args) throws Exception {
-        Main main = new Main();
-        main.configure().addRoutesBuilder(new MyRouteBuilder());
-        main.run(args);
+        System.out.println("Hello world!");
     }
 
 }
