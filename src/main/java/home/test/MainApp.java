0e1e13c41b120388b0b16c1cb8e4c16347bfee8d
Dmitri Baskakov <dmitri.baskakov.git@gmail.com>
2021-01-15 21:00:32 +0700

    Add StringWraper.equals()

--- src/main/java/home/test/MainApp.java
+++ src/main/java/home/test/MainApp.java
@@ -14,17 +14,33 @@ public class MainApp {
         System.out.println("source a : " + a.string);
         StringWraper b = new StringWraper("bbb");
         System.out.println("source b : " + b.string);
+        StringWraper c = new StringWraper("bbb");
 
         Swap(a, b);
         System.out.println("swap a <-> b");
         System.out.println("result a : " + a.string);
         System.out.println("result b : " + b.string);
+
+        if (a.equals(c)) {
+            System.out.println("a equals c");
+        } else {
+            System.out.println("a not equals c");
+        }
     }
 
     public static class StringWraper {
         public String string;
         StringWraper (String in) {
             string = in;
+        }
+
+        public boolean equals(Object other) {
+            if (other == this) return true;
+            if (other == null) return false;
+            if (!(other instanceof StringWraper)) return false;
+            StringWraper testOther = (StringWraper) other;
+            return this.string != null
+                    && this.string.equals(testOther.string);
         }
     }
 
