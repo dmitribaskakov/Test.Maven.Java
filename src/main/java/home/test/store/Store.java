b5fbb3c5c93f24a44f5e060eb4f997c5d6962fba
Dmitri Baskakov <dmitri.baskakov.git@gmail.com>
2021-03-17 00:31:04 +0700

    Chg package home.test.store over concurrent.locks.Condition

--- src/main/java/home/test/store/Store.java
+++ src/main/java/home/test/store/Store.java
@@ -1,28 +1,76 @@
 package home.test.store;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
 
 // Класс Магазин, хранящий произведенные товары
 public class Store {
     @SuppressWarnings("FieldCanBeLocal")
     final private int maxProduct = 3;
     private int product = 0;
-    public synchronized void get() {
-        while (product<1) {
-            try {
-                wait();
-            } catch (InterruptedException e) {}
-        }
-        product--;
-        System.out.println("Покупатель " + Thread.currentThread().getName() + " купил 1 товар. Товаров на складе: " + product);
-        notify();
+
+//    public synchronized void get() {
+//        while (product<1) {
+//            try {
+//                wait();
+//            } catch (InterruptedException e) {}
+//        }
+//        product--;
+//        System.out.println("Покупатель " + Thread.currentThread().getName() + " купил 1 товар. Товаров на складе: " + product);
+//        notify();
+//    }
+//    public synchronized void put() {
+//        while (product>=maxProduct) {
+//            try {
+//                wait();
+//            } catch (InterruptedException e) {}
+//        }
+//        product++;
+//        System.out.println("Производитель " + Thread.currentThread().getName() + " добавил 1 товар. Товаров на складе: " + product);
+//        notify();
+//    }
+
+    ReentrantLock locker;
+    Condition condition;
+
+    Store(){
+        locker = new ReentrantLock(); // создаем блокировку
+        condition = locker.newCondition(); // получаем условие, связанное с блокировкой
     }
-    public synchronized void put() {
-        while (product>=maxProduct) {
-            try {
-                wait();
-            } catch (InterruptedException e) {}
+
+    public void get() {
+        locker.lock();
+        try {
+            // пока нет доступных товаров на складе, ожидаем
+            while (product<1)
+                condition.await();
+
+            product--;
+            System.out.println("Покупатель " + Thread.currentThread().getName() + " купил 1 товар. Товаров на складе: " + product);
+
+            // сигнализируем
+            condition.signalAll();
+        } catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        } finally{
+            locker.unlock();
         }
-        product++;
-        System.out.println("Производитель " + Thread.currentThread().getName() + " добавил 1 товар. Товаров на складе: " + product);
-        notify();
+    }
+    public void put() {
+
+        locker.lock();
+        try {
+            // пока на складе maxProduct товара, ждем освобождения места
+            while (product>=maxProduct)
+                condition.await();
+
+            product++;
+            System.out.println("Производитель " + Thread.currentThread().getName() + " добавил 1 товар. Товаров на складе: " + product);
+            // сигнализируем
+            condition.signalAll();
+        } catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        } finally{
+            locker.unlock();
+        }
     }
 }
